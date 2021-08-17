5a9564d0088b3924a7b0b9a930f8447f908aa7ca
zhouyixun <291028775@qq.com>
2021-08-17 21:23:17 +0800

    controller_and_interface_change

---
+++ sonic-server-controller/src/main/java/com/sonic/control/models/interfaces/BugsType.java
@@ -0,0 +1,11 @@
+package com.sonic.control.models.interfaces;
+
+/**
+ * @author ZhouYiXun
+ * @des 定义类型
+ * @date 2021/8/15 19:30
+ */
+public interface BugsType {
+    int CRASH = 1; //崩溃
+    int LAG = 2; //卡顿
+}
