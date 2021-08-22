9c3bded5b73bf230da39f0644dbe915cf2f645f9
zhouyixun <291028775@qq.com>
2021-08-22 12:01:12 +0800

    save_task_1

--- sonic-server-controller/src/main/java/com/sonic/controller/ControllerApplication.java
+++ sonic-server-controller/src/main/java/com/sonic/controller/ControllerApplication.java
@@ -3,6 +3,7 @@ package com.sonic.controller;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 /**
@@ -12,6 +13,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
  */
 @SpringBootApplication(scanBasePackages = {"com/sonic/controller", "com/sonic/common"})
 @EnableJpaAuditing
+@EnableFeignClients
 @EnableEurekaClient
 public class ControllerApplication {
     public static void main(String[] args) {
