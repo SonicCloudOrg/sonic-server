1165caea1ccf67b08a2aa0772624d8c138db1cbb
ZhouYixun <291028775@qq.com>
2022-06-01 23:02:29 +0800

    修复历史遗留问题

--- sonic-server-common/src/main/java/org/cloud/sonic/common/feign/FolderFeignClient.java
+++ sonic-server-common/src/main/java/org/cloud/sonic/common/feign/FolderFeignClient.java
@@ -4,10 +4,7 @@ import org.cloud.sonic.common.feign.fallback.FolderFeignClientFallBack;
 import org.cloud.sonic.common.http.RespModel;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
@@ -15,7 +12,7 @@ import java.io.IOException;
 @FeignClient(value = "sonic-server-folder", fallback = FolderFeignClientFallBack.class)
 public interface FolderFeignClient {
 
-    @GetMapping("/files/delete")
+    @DeleteMapping("/files")
     RespModel<String> delete(@RequestParam(name = "day") int day);
 
 
