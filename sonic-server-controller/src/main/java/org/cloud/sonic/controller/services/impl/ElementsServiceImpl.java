a2b3951bf2ff4d4315f8302c9b563ac8e8523ba0
dongxiaoqiang <1193272767@qq.com>
2022-11-27 21:27:56 +0800

    fix:代码优化

--- sonic-server-controller/src/main/java/org/cloud/sonic/controller/services/impl/ElementsServiceImpl.java
+++ sonic-server-controller/src/main/java/org/cloud/sonic/controller/services/impl/ElementsServiceImpl.java
@@ -90,7 +90,7 @@ public class ElementsServiceImpl extends SonicServiceImpl<ElementsMapper, Elemen
     }
 
     @Transactional
-    privaet ElementsDTO findEleDetail(Elements elements) {
+    private ElementsDTO findEleDetail(Elements elements) {
         if (elements.getModuleId() != null && elements.getModuleId() != 0) {
             Modules modules = modulesMapper.selectById(elements.getModuleId());
             if (modules != null) {
