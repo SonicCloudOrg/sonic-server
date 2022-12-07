af30982f5a810d04180dc4847c1aad3425178bf2
ZhouYixun <291028775@qq.com>
2022-12-07 23:09:39 +0800

    fix: init job type

--- sonic-server-controller/src/main/java/org/cloud/sonic/controller/config/mybatis/ClearForeignKey.java
+++ sonic-server-controller/src/main/java/org/cloud/sonic/controller/config/mybatis/ClearForeignKey.java
@@ -41,49 +41,49 @@ public class ClearForeignKey implements ApplicationListener<DataSourceSchemaCrea
 
     @Override
     public void onApplicationEvent(DataSourceSchemaCreatedEvent event) {
-//        DataSource dataSource = (DataSource) event.getSource();
-//        String dataBase = "";
+        DataSource dataSource = (DataSource) event.getSource();
+        String dataBase = "";
 //        String findFKSql = "SELECT CONCAT('ALTER TABLE ', TABLE_NAME,' DROP FOREIGN KEY ',CONSTRAINT_NAME) as ddl " +
 //                "FROM information_schema.TABLE_CONSTRAINTS c " +
 //                "WHERE c.TABLE_SCHEMA='%s' AND c.CONSTRAINT_TYPE='FOREIGN KEY'";
-//        String transSql = "UPDATE QRTZ_JOB_DETAILS set JOB_CLASS_NAME='org.cloud.sonic.controller.quartz.QuartzJob'";
-//        List<String> deleteSqlList = new ArrayList<>();
-//        try (Connection connection = dataSource.getConnection()) {
-//            try (Statement statement = connection.createStatement()) {
-//
-//                // 获取当前数据库名
-//                ResultSet resultSet1 = statement.executeQuery("select DATABASE() db");
-//                if (resultSet1.next()) {
-//                    dataBase = resultSet1.getString("db");
-//                }
-//
+        String transSql = "UPDATE jobs SET type = 'testJob' WHERE type is null or type = \"\"";
+        List<String> deleteSqlList = new ArrayList<>();
+        try (Connection connection = dataSource.getConnection()) {
+            try (Statement statement = connection.createStatement()) {
+
+                // 获取当前数据库名
+                ResultSet resultSet1 = statement.executeQuery("select DATABASE() db");
+                if (resultSet1.next()) {
+                    dataBase = resultSet1.getString("db");
+                }
+
 //                // 查询所有外键索引，并拼装成删除sql
 //                ResultSet resultSet2 = statement.executeQuery(String.format(findFKSql, dataBase));
 //                while (resultSet2.next()) {
 //                    deleteSqlList.add(resultSet2.getString("ddl"));
 //                }
-//
-//                // 版本兼容sql
-//                Boolean resultSet3 = statement.execute(String.format(transSql, dataBase));
-//                if (!resultSet3) {
-//                    log.info(String.format("transfer sql %s failed, ignore...", transSql));
-//                }
-//
-//                // 执行删除外键sql
-//                for (String deleteSql : deleteSqlList) {
-//                    statement.executeUpdate(deleteSql);
-//                }
-//
+
+                // 版本兼容sql
+                Boolean resultSet3 = statement.execute(String.format(transSql, dataBase));
+                if (!resultSet3) {
+                    log.info(String.format("transfer sql %s failed, ignore...", transSql));
+                }
+
+                // 执行删除外键sql
+                for (String deleteSql : deleteSqlList) {
+                    statement.executeUpdate(deleteSql);
+                }
+
 //                // 删除 test_suites_devices 表的主键
 //                try {
 //                    statement.executeUpdate("Alter table test_suites_devices Drop primary key");
 //                } catch (Exception e) {
 //                    // 无视错误
 //                }
-//            }
-//        } catch (Exception e) {
-//            log.error("clear foreign key failed.");
-//            e.printStackTrace();
-//        }
+            }
+        } catch (Exception e) {
+            log.error("clear foreign key failed.");
+            e.printStackTrace();
+        }
     }
 }
