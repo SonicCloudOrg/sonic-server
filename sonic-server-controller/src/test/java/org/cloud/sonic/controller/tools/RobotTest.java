d1066f6f86b9bc2cecfc4ec251c5d6dac0b5de3c
ZhouYixun <291028775@qq.com>
2023-01-20 01:36:25 +0800

    feat: add unit test

--- sonic-server-controller/src/test/java/org/cloud/sonic/controller/tools/RobotTest.java
+++ sonic-server-controller/src/test/java/org/cloud/sonic/controller/tools/RobotTest.java
@@ -1,113 +1,119 @@
-//package org.cloud.sonic.controller.tools;
-//
-//import org.cloud.sonic.controller.BaseUnit;
-//import org.cloud.sonic.controller.models.interfaces.RobotType;
-//import org.junit.jupiter.api.Test;
-//import org.springframework.beans.factory.annotation.Autowired;
-//
-//public class RobotTest extends BaseUnit {
-//
-//    @Autowired
-//    RobotMsgTool robotMsgTool;
-//
-//    private String wechatToken = "https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=xxx";
-//
-//    private String feishuToken = "https://open.feishu.cn/open-apis/bot/v2/hook/xxx";
-//
-//    // API path https://api.telegram.org/bot<token>/sendMessage?chat_id=<chat_id>
-//    private String telegramToken = "https://api.telegram.org/botxxx/sendMessage?chat_id=zzz";
-//
-//    private String lineToken = "XXXXX";
+package org.cloud.sonic.controller.tools;
 
-//    @Test
-//    public void sendDayReportMessage() {
-//        robotMsgTool.sendDayReportMessage(wechatToken, "" ,1, "2",
-//                "1", "2" , 1,2,3, RobotType.WeChat);
-//    }
-//
-//    @Test
-//    public void sendDayReportMessage2() {
-//        robotMsgTool.sendDayReportMessage(wechatToken, "" ,1, "2",
-//                "1", "2" , 1,0,0, RobotType.WeChat);
-//    }
-//
-//    @Test
-//    public void testSendErrorDevice1() {
-//        robotMsgTool.sendErrorDevice(wechatToken, "", RobotType.WeChat, 1, 80,"111");
-//    }
-//
-//
-//    @Test
-//    public void testSendErrorDevice2() {
-//        robotMsgTool.sendErrorDevice(wechatToken, "", RobotType.WeChat, 2, 80,"111");
-//    }
-//
-//    @Test
-//    public void testSendErrorDeviceFeishu() {
-//        robotMsgTool.sendErrorDevice(feishuToken, "", RobotType.FeiShu, 1, 80,"测试");
-//    }
-//
-//    @Test
-//    public void testSendErrorDeviceFeishu2() {
-//        robotMsgTool.sendErrorDevice(feishuToken, "", RobotType.FeiShu, 2, 80,"测试");
-//    }
-//
-//
-//    @Test
-//    public void sendWeekReportMessage() {
-//        robotMsgTool.sendWeekReportMessage(wechatToken, "" ,1, "2",
-//                "1", "2" , 1,0,0,100, RobotType.WeChat);
-//    }
-//
-//    @Test
-//    public void sendResultFinishReport() {
-//        robotMsgTool.sendResultFinishReport(wechatToken, "","111", 1,1,1,1,
-//                1,RobotType.WeChat);
-//    }
-//
-//    @Test
-//    public void sendDayReportMessageForTelegram() {
-//        robotMsgTool.sendDayReportMessage(telegramToken, "" ,1, "2",
-//                "1", "2" , 1,2,3, RobotType.Telegram);
-//    }
-//
-//    @Test
-//    public void sendResultFinishReportForTelegram() {
-//        robotMsgTool.sendResultFinishReport(telegramToken, "","111", 1,1,1,1,
-//                1,RobotType.Telegram);
-//    }
-//
-//    @Test
-//    public void sendWeekReportMessageForTelegram() {
-//        robotMsgTool.sendWeekReportMessage(telegramToken, "" ,1, "2",
-//                "1", "2" , 1,0,0,100, RobotType.Telegram);
-//    }
-//
-//    @Test
-//    public void testSendErrorDeviceForTelegram() {
-//        robotMsgTool.sendErrorDevice(telegramToken, "", RobotType.Telegram, 2, 80,"测试");
-//    }
+import org.cloud.sonic.controller.ControllerApplication;
+import org.cloud.sonic.controller.models.interfaces.RobotType;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
-//    @Test
-//    public void sendDayReportMessageForLineNotify() {
-//        robotMsgTool.sendDayReportMessage(lineToken, "" ,1, "2",
-//                "1", "2" , 1,2,3, RobotType.LineNotify);
-//    }
-//    @Test
-//    public void sendResultFinishReportForLineNotify() {
-//        robotMsgTool.sendResultFinishReport(lineToken, "","111", 1,1,2,1,
-//                1,RobotType.LineNotify);
-//    }
-//
-//    @Test
-//    public void sendWeekReportMessageForLineNotify() {
-//        robotMsgTool.sendWeekReportMessage(lineToken, "" ,1, "2",
-//                "1", "2" , 1,3,5,100, RobotType.LineNotify);
-//    }
-//
-//    @Test
-//    public void testSendErrorDeviceForLineNotify() {
-//        robotMsgTool.sendErrorDevice(lineToken, "", RobotType.LineNotify, 2, 80,"测试");
-//    }
+@SpringBootTest(classes = ControllerApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@RunWith(SpringRunner.class)
+public class RobotTest {
 
-//}
+    @Autowired
+    private RobotMsgTool robotMsgTool;
+
+    private String wechatToken = "https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=xxx";
+
+    private String feishuToken = "https://open.feishu.cn/open-apis/bot/v2/hook/xxx";
+
+    // API path https://api.telegram.org/bot<token>/sendMessage?chat_id=<chat_id>
+    private String telegramToken = "https://api.telegram.org/botxxx/sendMessage?chat_id=zzz";
+
+    private String lineToken = "XXXXX";
+
+    @Test
+    public void sendDayReportMessage() {
+        robotMsgTool.sendDayReportMessage(wechatToken, "", 1, "2",
+                "1", "2", 1, 2, 3, RobotType.WeChat);
+    }
+
+    @Test
+    public void sendDayReportMessage2() {
+        robotMsgTool.sendDayReportMessage(wechatToken, "", 1, "2",
+                "1", "2", 1, 0, 0, RobotType.WeChat);
+    }
+
+    @Test
+    public void testSendErrorDevice1() {
+        robotMsgTool.sendErrorDevice(wechatToken, "", RobotType.WeChat, 1, 80, "111");
+    }
+
+
+    @Test
+    public void testSendErrorDevice2() {
+        robotMsgTool.sendErrorDevice(wechatToken, "", RobotType.WeChat, 2, 80, "111");
+    }
+
+    @Test
+    public void testSendErrorDeviceFeishu() {
+        robotMsgTool.sendErrorDevice(feishuToken, "", RobotType.FeiShu, 1, 80, "测试");
+    }
+
+    @Test
+    public void testSendErrorDeviceFeishu2() {
+        robotMsgTool.sendErrorDevice(feishuToken, "", RobotType.FeiShu, 2, 80, "测试");
+    }
+
+
+    @Test
+    public void sendWeekReportMessage() {
+        robotMsgTool.sendWeekReportMessage(wechatToken, "", 1, "2",
+                "1", "2", 1, 0, 0, 100, RobotType.WeChat);
+    }
+
+    @Test
+    public void sendResultFinishReport() {
+        robotMsgTool.sendResultFinishReport(wechatToken, "", "111", 1, 1, 1, 1,
+                1, RobotType.WeChat);
+    }
+
+    @Test
+    public void sendDayReportMessageForTelegram() {
+        robotMsgTool.sendDayReportMessage(telegramToken, "", 1, "2",
+                "1", "2", 1, 2, 3, RobotType.Telegram);
+    }
+
+    @Test
+    public void sendResultFinishReportForTelegram() {
+        robotMsgTool.sendResultFinishReport(telegramToken, "", "111", 1, 1, 1, 1,
+                1, RobotType.Telegram);
+    }
+
+    @Test
+    public void sendWeekReportMessageForTelegram() {
+        robotMsgTool.sendWeekReportMessage(telegramToken, "", 1, "2",
+                "1", "2", 1, 0, 0, 100, RobotType.Telegram);
+    }
+
+    @Test
+    public void testSendErrorDeviceForTelegram() {
+        robotMsgTool.sendErrorDevice(telegramToken, "", RobotType.Telegram, 2, 80, "测试");
+    }
+
+    @Test
+    public void sendDayReportMessageForLineNotify() {
+        robotMsgTool.sendDayReportMessage(lineToken, "", 1, "2",
+                "1", "2", 1, 2, 3, RobotType.LineNotify);
+    }
+
+    @Test
+    public void sendResultFinishReportForLineNotify() {
+        robotMsgTool.sendResultFinishReport(lineToken, "", "111", 1, 1, 2, 1,
+                1, RobotType.LineNotify);
+    }
+
+    @Test
+    public void sendWeekReportMessageForLineNotify() {
+        robotMsgTool.sendWeekReportMessage(lineToken, "", 1, "2",
+                "1", "2", 1, 3, 5, 100, RobotType.LineNotify);
+    }
+
+    @Test
+    public void testSendErrorDeviceForLineNotify() {
+        robotMsgTool.sendErrorDevice(lineToken, "", RobotType.LineNotify, 2, 80, "测试");
+    }
+
+}
