466989a7246e6f21d17595139392247bb7105e83
Eason <291028775@qq.com>
2023-03-20 21:52:30 +0800

    fix: database not init

---
+++ sonic-server-controller/src/main/java/org/cloud/sonic/controller/config/mybatis/ActableConfig.java
@@ -0,0 +1,37 @@
+/*
+ *   sonic-server  Sonic Cloud Real Machine Platform.
+ *   Copyright (C) 2022 SonicCloudOrg
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU Affero General Public License as published
+ *   by the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU Affero General Public License for more details.
+ *
+ *   You should have received a copy of the GNU Affero General Public License
+ *   along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+package org.cloud.sonic.controller.config.mybatis;
+
+import com.gitee.sunchenbin.mybatis.actable.manager.handler.StartUpHandler;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+
+@Configuration
+@Order(1)
+public class ActableConfig {
+
+    @Autowired
+    private StartUpHandler startUpHandler;
+
+    @Bean
+    public void generate() {
+        startUpHandler.startHandler();
+    }
+}
