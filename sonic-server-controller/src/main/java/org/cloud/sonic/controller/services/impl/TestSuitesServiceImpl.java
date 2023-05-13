7abd7d1e1c2ffcd1130896962de8df13363cebcc
caofengbin <1050430934@qq.com>
2023-05-13 18:33:00 +0800

    fix，删除单个测试套件时，先清理掉test_suites_test_cases 以及 test_suites_devices 两表中的记录

--- sonic-server-controller/src/main/java/org/cloud/sonic/controller/services/impl/TestSuitesServiceImpl.java
+++ sonic-server-controller/src/main/java/org/cloud/sonic/controller/services/impl/TestSuitesServiceImpl.java
@@ -360,6 +360,11 @@ public class TestSuitesServiceImpl extends SonicServiceImpl<TestSuitesMapper, Te
 
     @Override
     public boolean delete(int id) {
+        // 先删除 test_suites_test_cases 以及 test_suites_devices 两表中的记录
+        testSuitesTestCasesMapper.delete(new LambdaQueryWrapper<TestSuitesTestCases>()
+                .eq(TestSuitesTestCases::getTestSuitesId, id));
+        testSuitesDevicesMapper.delete(new LambdaQueryWrapper<TestSuitesDevices>()
+                .eq(TestSuitesDevices::getTestSuitesId, id));
         return baseMapper.deleteById(id) > 0;
     }
 
